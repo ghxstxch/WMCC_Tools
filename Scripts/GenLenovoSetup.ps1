0fcd6524ca61c9ad7f937704ac12649533827272
ghxstxch <107282306+ghxstxch@users.noreply.github.com>
2023-08-01 09:19:30 -0500

    Fix slow downloads - GenLenovoSetup.ps1
    Fixed slow downloads by adding $ProgressPreference = 'SilentlyContinue' 
    
    This removes the progress bar - on older versions of Powershell, the bar updates byte by byte slowing the downloads immensely 
    
    Signed-off-by: ghxstxch <107282306+ghxstxch@users.noreply.github.com>

--- Scripts/GenLenovoSetup.ps1
+++ Scripts/GenLenovoSetup.ps1
@@ -2,19 +2,22 @@
 Set-ExecutionPolicy RemoteSigned
 
 
-$Installer = $env:TEMP + "\chrome_installer.exe"; 
+$Installer = $env:TEMP + "\chrome_installer.exe";
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Installer; 
 Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
 Remove-Item $Installer;
 
 
-$Installer = $env:TEMP + "\adobe_installer.exe"; 
+$Installer = $env:TEMP + "\adobe_installer.exe";
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300320201/AcroRdrDC2300320201_en_US.exe" -OutFile $Installer; 
 Start-Process -FilePath $Installer -Args "/sAll /a /qn" -Verb RunAs -Wait; 
 Remove-Item $Installer;
 
 
-$Installer = $env:TEMP + "\LSU_installer.exe"; 
+$Installer = $env:TEMP + "\LSU_installer.exe";
+$ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest "https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.08.01.exe" -OutFile $Installer; 
 Start-Process -FilePath $Installer -Args "/silent /install" -Verb RunAs -Wait; 
 Remove-Item $Installer;
