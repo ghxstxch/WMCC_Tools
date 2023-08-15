22915d4638ed8bd427ab6fba713de98e736cf78d
ghxstxch <107282306+ghxstxch@users.noreply.github.com>
2023-08-15 15:52:16 -0500

    Update GenLenovoSetup.bat - Script cleanup
    Cleaned up user-facing script a bit, @ECHO Off added to stop commands from being shown, @ECHO'd status changes to script. 
    
    Signed-off-by: ghxstxch <107282306+ghxstxch@users.noreply.github.com>

--- GenLenovoSetup.bat
+++ GenLenovoSetup.bat
@@ -1,4 +1,10 @@
+@ECHO off
+@ECHO Setting working directory
 cd /d %~dp0
+
+@ECHO Applying settings
 powershell Set-ExecutionPolicy RemoteSigned
 powershell -file .\Scripts\Settings.ps1
+
+@ECHO Installing applications
 powershell -file .\Scripts\GenLenovoSetup.ps1
