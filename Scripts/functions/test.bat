223cf7f2af0dae368988df771a8bdd460e3a10ee
ghxstxch <tyoung@skidbaby.org>
2023-08-28 13:24:44 -0500

    Update test.bat

--- Scripts/functions/test.bat
+++ Scripts/functions/test.bat
@@ -1,16 +1,21 @@
 @ECHO OFF
 :start
-goto execpol
+goto unblockfiles
 
-:execpol
-FOR /F "tokens=* USEBACKQ" %%F IN (`powershell Get-ExecutionPolicy`) DO (
-SET execpol=%%F
-)
-powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
-ECHO The current Execution-Policy will be setset to Bypass.
-ECHO This will be set back to the original value, %execpol% at the end of this script.
+:unblockfiles
+dir -Recurse | Unblock-File
+
+#:execpol
+#FOR /F "tokens=* USEBACKQ" %%F IN (`powershell Get-ExecutionPolicy`) DO (
+#SET execpol=%%F
+#)
+#powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+#ECHO The current Execution-Policy will be setset to Bypass.
+#ECHO This will be set back to the original value, %execpol% at the end of this script.
 
 :time
 powershell Set-TimeZone -Id "Central Standard Time"
+
+
 
 :end
